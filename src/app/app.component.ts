f4d342ec9d921ff6860a0a79ce3605f3945ea86b
Leonel Adrian Bruzzese <lbruzzese@HCJSM.local>
2024-03-06 09:37:13 -0300

    intento 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,52 @@
+// import { Component } from '@angular/core';
+// import { LibrosService } from './libros.service';
+// import { NgModel } from '@angular/forms';
+
+// export interface Biblioteca {
+//   titulo: string, 
+//   autor: string, 
+//   género: string,
+//   añoDePublicación: number,
+//   disponible: boolean
+// }
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'Libreria';
+
+//   titulo: string = '';
+
+//   biblioteca: Biblioteca[] = [];
+
+//   constructor(private librosService: LibrosService) { 
+//   }
+
+//   buscarLibro() {
+//     this.biblioteca = this.librosService.biblioteca_modulo().filter((bilbioteca) => bilbioteca.titulo === this.titulo);
+
+//     if (this.biblioteca.length > 0) {
+//       this.biblioteca;
+//     } else {
+//       alert("El libro no existe");
+//     }
+//   }
+
+//   prestarLibro() {
+    
+//   }
+
+
+
+// }
+
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Inject, ViewChild, ElementRef, OnInit } from '@angular/core'; // rendered2
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +54,38 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ejercitando';
+  title = 'formgroup';
+
+  public permisosForm: any;
+
+  startForm() {
+    this.permisosForm = new FormGroup ({
+      permisos: new FormArray([
+      ])
+    })
+
+    this.agregarFilaPermisos();
+  }
+  
+
+  get permisos(): FormArray {
+    return this.permisosForm.get('permisos') as FormArray;
+  } 
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.startForm();
+  }
+
+  agregarFilaPermisos() {
+    this.permisos.push(new FormControl(''));
+  }
+
+  borrarFilaPermisos(index:number): void {
+    this.permisos.removeAt(index);
+  }
+
 }
+
---
+++ src/app/libros.service.ts
@@ -0,0 +1,40 @@
+// import { Injectable } from '@angular/core';
+// import { Biblioteca } from './app.component';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class LibrosService {
+
+//   constructor() { }
+
+//   biblioteca: Biblioteca[] = [
+//     { 
+//       titulo: "Lobos", 
+//       autor: "Stephen King", 
+//       género: "Terror",
+//       añoDePublicación: 2002,
+//       disponible: true
+//     },
+//     {
+//       titulo: "Mr Hyde", 
+//       autor: "Conan Doyle", 
+//       género: "Suspenso",
+//       añoDePublicación: 1963,
+//       disponible: true
+//     },
+//     {
+//       titulo: "El Señor De Los Anillos", 
+//       autor: "Tolkien", 
+//       género: "Fantasía",
+//       añoDePublicación: 1952,
+//       disponible: true
+//     }
+//   ]
+
+//   biblioteca_modulo() {
+//     return this.biblioteca;
+//   }
+
+
+// }
